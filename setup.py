47d3118506dbdec30a4382b25f29850bcadb8a09
Saksham Pathak <msa24021@iiitl.ac.in>
2025-02-11 17:26:53 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='just a short demo of cookiecutter , dvc and git push',
+    author='Saksham Pathak',
+    license='',
+)
